02b87fda0e036fb89e75e98c9cf7720f82b50381
wakawaka <wakawaka.427@gmail.com>
2015-11-14 18:03:56 +0900

    initialize

---
+++ app/src/main/java/codefortoyama/jimdo/com/lifestyleconcierge/SplashActivity.java
@@ -0,0 +1,34 @@
+package codefortoyama.jimdo.com.lifestyleconcierge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1500);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+}
